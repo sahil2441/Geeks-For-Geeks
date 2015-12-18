6428b8281637026ab2cc81bcc9f5caf2dc0f2b86
Sahil Jain <sahil.2441@gmail.com>
2015-12-17 20:59:48 -0500

    Added few more programs.

--- FirstProject/src/CountNumberOfBinaryStrings.java
+++ FirstProject/src/CountNumberOfBinaryStrings.java
@@ -1,24 +1,25 @@
 /**
- * Second Edit
- * After Push
- * Count number of binary strings without consecutive 1’s
+ * Second Edit After Push Count number of binary strings without consecutive 1’s
+ * 
  * @author sahil
  */
 public class CountNumberOfBinaryStrings {
 
 	public static void main(String[] args) {
-		int n=2;
-		int a[]=new int[n];
-		int b[]=new int[n];
-		
-		a[0]=1;b[0]=1;
-		int i=1;
-		while(i<n){
-			a[i]=a[i-1]+b[i-1];
-			b[i]=a[i-1];
+
+		int n = 2;
+		int a[] = new int[n];
+		int b[] = new int[n];
+
+		a[0] = 1;
+		b[0] = 1;
+		int i = 1;
+		while (i < n) {
+			a[i] = a[i - 1] + b[i - 1];
+			b[i] = a[i - 1];
 			i++;
 		}
-		System.out.println(a[n-1]+b[n-1]);
+		System.out.println(a[n - 1] + b[n - 1]);
 	}
 
 }
---
+++ FirstProject/src/Rought.java
@@ -0,0 +1,11 @@
+
+public class Rought {
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = a;
+		b++;
+		System.out.println(b);
+	}
+
+}
